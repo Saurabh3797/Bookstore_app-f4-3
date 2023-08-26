653266d99da8b16344941cd65ab55f79161602b1
Saurabh3797 <128299348+Saurabh3797@users.noreply.github.com>
2023-08-26 21:49:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import Navbar from "./Components/Navbar";
+import BookList from "./Components/BookList";
+//import BookDetails from "./Components/BookDetails";
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+function App() {
+
+  const [books,setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+   useEffect(()=>{
+    const handleSearch = async (queryyy) => {
+       
+       
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${queryyy}`
+        );
+  
+        const searchResult = response.data.items;
+        
+        setBooks(searchResult);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+      }
+    };
+    console.log(searchQuery,"showww")
+    handleSearch({searchQuery})
+  },[searchQuery])
+  
+
+  useEffect(()=>{
+    
+    const DataFetch = async ()=>{
+
+      try{
+        const resultsHerry = await axios.get("https://www.googleapis.com/books/v1/volumes?q=harry+potter");
+        const resultsSharelock = await axios.get("https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes"); 
+        const AllData = resultsHerry.data.items.concat(resultsSharelock.data.items)
+        // console.log(AllData)
+        setBooks(()=>AllData);
+      }
+      catch(error){console.log(error,"error Fetching time.....")}
+    }
+    DataFetch()
+  },[]);
+  
+  // console.log(books,"bokesdfas",books.length>1)
+  return (
+    <div className="App">
+       
+      <Navbar onSearch={setSearchQuery}/>
+      {books.length>0&&
+        <BookList books={books}/>
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import log from "../images/Group.png";
+import KeazoNBOOKS from "../images/KeazoNBOOKS.png";
+//import Group from "../images/Group.png";
+import heart from "../images/heart.png";
+import bell from "../images/notifications.png";
+import search from "../images/search.png";
+import pro from "../images/stone.png";
+import profile from "../images/profile.png";
+//import SearchBar from "./SearchBar";
+import searchImageicon from "../images/search.png"; 
+
+function Navbar({ onSearch }) {
+    const [query,setQuery] =  useState("")
+
+    const handleSearch = (e) => {
+      e.preventDefault();
+      onSearch({query});
+    };
+  return (
+    <nav className="navbar">
+      <div className="log-div">
+        <img src={log} alt="log" className="log image-nav" />
+        <img src={KeazoNBOOKS} alt="log-name" className="log-name image-nav" />
+      </div>
+      {/* <SearchBar onSearch2 ={onSearch}/> */}
+      <form className="searchbar-div" onSubmit={handleSearch}>
+        <div className="search-bar">
+          <img src={searchImageicon} alt="search" className="search-icon-img" />
+          <input
+            type="text"
+            onChange={(e) => setQuery(e.target.value)}
+            className="search-input"
+            placeholder="Search for the book you want and read it now... Sherlock Holmes, Harry Pot..."
+          />
+        </div>
+        <div className="search-button">
+          <button className="search-btn">Search</button>
+        </div>
+      </form>
+      <div className="icons-div">
+        <div className="icon-div icon-div-1">
+          <img src={heart} alt="book" className="book image-nav" />
+        </div> 
+        <div className="icon-div icon-div-2">
+          <img src={bell} alt="notification" className="bell image-nav" />
+        </div>
+        <div className="icon-div icon-div-3">
+          <img src={pro} alt="pro" className="pro image-nav" />
+        </div>
+        <div className="icon-div icon-div-4">
+          <img src={profile} alt="profile" className="profile image-nav" />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
